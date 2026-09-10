0ccb37ce4188ec0c49cf23b8adef72252542434b
Dipak-Natureglobal <dipak.mourya@inworkglobal.com>
2026-09-10 13:00:57 +0530

    refactor: remove AuthGate wrapper from main rendering

--- apps/refinance-prototype/src/main.tsx
+++ apps/refinance-prototype/src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AuthGate } from './shell/AuthGate.tsx'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
 
 createRoot(rootElement).render(
   <StrictMode>
-    <AuthGate>
-      <App />
-    </AuthGate>
+    <App />
   </StrictMode>
 )
